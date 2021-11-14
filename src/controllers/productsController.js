9b0564270d07569664ae717e28c8ddb4af036031
Elias-Zurita <eliass.zurita@gmail.com>
2021-11-14 15:08:11 -0300

    base de datos funcionando

--- src/controllers/productsController.js
+++ src/controllers/productsController.js
@@ -2,41 +2,40 @@ const {validationResult} = require("express-validator"); // requiere la libreria
 const db = require ("../db/models"); // nos permite utilizar la base de datos
 
 const productsController = {
-    create: function (req, res) {
+    crear: function (req, res) {
         db.Deporte.findAll()               // obtiene todos los Deportes (alias del modelo Deporte)
         .then(function(deportes){      // despues que obtenga los deportes
             return res.render("products/create", {deportes:deportes});   // renderiza la vista de creacion de productos
         })
     },
 
-    store: function (req, res) {
+    guardado: function (req, res) {
         const resultValidation = validationResult(req); // Campos que tuvieron error //
-		if (resultValidation.errors.length > 0) { // Si resultValidation es mayor a cero (tiene errores) renderizo el formulario de create de nuevo //
+		if (resultValidation.errors.length > 0) { // Si resultValidation es mayor a cero (tiene errores) renderizo el formulario de create de nuevo 
 			return res.render('products/create', {
-				errors: resultValidation.mapped(),  // Le pasa a la vista de create los errores que se señalaron en validateProductsMiddleware //
+				errors: resultValidation.mapped(),  // Le pasa a la vista de create los errores que se señalaron en validateProductsMiddleware 
                 oldData:req.body
             });
                 }
         db.Producto.create({
-            id: products.length + 1,  // cuenta la cantidad de elementos que se tienen y le suma uno //
-            img: req.file.filename,
-            nombre: req.body.nombre,
-            descripcion: req.body.descripcion,
-            categoria: req.body.categoria,
-            marca: req.body.marca,
+            nombre: req.body.nombre, 
             precio: req.body.precio,
-            genero: req.body.genero,
-            deporte: req.body.deporte,
-            talle: req.body.talle
+            descripcion: req.body.descripcion,
+            imagen: req.file.imagen,
+            talle: req.body.talle_id, // talle es el name que tiene en el formulario de creacion y talle_id es el nombre de la columna en el modelo
+            genero: req.body.genero_id,
+            deporte: req.body.deporte_id,
+            marca: req.body.marca_id,
+            categoria: req.body.categoria
         })
 
-        res.redirect("products/list");
+        res.redirect("/products/list");  // si no hay campos sin llenar redirecciona a list
     },
 
     list: function (req, res) {
         db.Producto.findAll()
             .then (function(productos) {
-                res.render ("productsList", {productos: productos})
+                res.render ("products/productsList", {productos: productos})
             })
     }
 }
--- src/routes/productRoutes.js
+++ src/routes/productRoutes.js
@@ -9,10 +9,10 @@ const uploadFile = require('../middlewares/multerMiddlewareProducts');
 const validations = require('../middlewares/validateProductsMiddleware');
 
 // Formulario de creacion de productos
-router.get("/create", productsController.create);
+router.get("/create", productsController.crear);
 
 // Procesa la creacion de productos
-router.post("/create", uploadFile.single("imagen"), validations, productsController.store);
+router.post("/create", uploadFile.single("imagen"), validations, productsController.guardado);
 
 // Formulario de listado de productos
 router.get("/list", productsController.list);
