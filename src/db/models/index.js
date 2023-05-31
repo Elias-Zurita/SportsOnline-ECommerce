5d88610a841d1aeafb0dc808db01a66828b15420
Elias-Zurita <eliass.zurita@gmail.com>
2023-05-31 20:16:43 -0300

    config db

--- src/db/models/index.js
+++ src/db/models/index.js
@@ -5,26 +5,28 @@ const path = require('path');
 const Sequelize = require('sequelize');
 const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
-const config = require(__dirname + '/../config/config.js')[env];
+const config = require(__dirname + '/../config/config.js')["production"];
 const db = {};
 
 let sequelize;
+sequelize = new Sequelize(config.database, config.username, config.password, {
+  host:config.host,
+  port: config.port,
+  dialect: config.dialect,
+  pool: { 
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000
+  }
+});
+/*
 if (config.use_env_variable) {
   sequelize = new Sequelize(process.env[config.use_env_variable], config);
 } else {
-  sequelize = new Sequelize(config.database, config.username, config.password, {
-    host:config.host,
-    port: config.port,
-    dialect: config.dialect,
-    pool: { 
-      max: 5,
-      min: 0,
-      acquire: 30000,
-      idle: 10000
-  }
-  });
+  
 }
-
+*/
 fs
   .readdirSync(__dirname)
   .filter(file => {
