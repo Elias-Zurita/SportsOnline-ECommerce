f5cbe3bec7a0e4f03340463ace0e941478badbad
Elias-Zurita <eliass.zurita@gmail.com>
2023-05-31 20:01:16 -0300

    config index db

--- src/db/models/index.js
+++ src/db/models/index.js
@@ -12,7 +12,17 @@ let sequelize;
 if (config.use_env_variable) {
   sequelize = new Sequelize(process.env[config.use_env_variable], config);
 } else {
-  sequelize = new Sequelize(config.database, config.username, config.password, ...config);
+  sequelize = new Sequelize(config.database, config.username, config.password, {
+    host:config.host,
+    port: config.port,
+    dialect: config.dialect,
+    pool: { 
+      max: 5,
+      min: 0,
+      acquire: 30000,
+      idle: 10000
+  }
+  });
 }
 
 fs
